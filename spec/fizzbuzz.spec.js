f67c41ce9ff65aa8173dbe7fc686232c1a1d6977
Owlbuddy <j.harding.911@live.com>
2015-10-08 10:27:49 -0600

    Second Commit

---
+++ spec/fizzbuzz.spec.js
@@ -0,0 +1,13 @@
+/*globals require, describe, it, expect*/
+
+// 'use strict';
+
+var fizzbuzz = require("../fizzbuzz.js");
+
+
+describe('fizz buzz', function() {
+it('should return 0 given 0', function() {
+    var result = fizzbuzz.getResult(0);
+    expect(result.toEqual(0));
+ });
+});
