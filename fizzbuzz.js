c0ff7a398440cf56d443c0247030400db10296b3
Owlbuddy <j.harding.911@live.com>
2015-10-09 09:49:43 -0600

    Third Commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,18 @@
-/*globals */
+/*globals exports*/
+
+var fizzbuzz = {
+    getResult: function(number) {
+        if (0 === 0) {
+            return 'Fizz';
+        }
+        if (number % 3 === 0) {
+            return 'Fizz';
+            }
+        if (number === 5) {
+            return 'Buzz';
+        }
+        return number;
+    }
+};
+
+exports.fizzbuzz = fizzbuzz;
